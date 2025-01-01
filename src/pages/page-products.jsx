af74d97170a45d64bc52fcccb980eb088bcdae55
johnbobelisa <johnelisaaa@gmail.com>
2025-01-01 22:44:41 +1100

    revamp v3

--- src/pages/page-products.jsx
+++ src/pages/page-products.jsx
@@ -2,14 +2,33 @@ import React from "react";
 import Navbar from "../components/navbar/navbar";
 import Sidebar from "../components/Sidebar/Sidebar";
 
-import ProductCard from "../components/products/ProductCard";
 import ProductListing from "../components/products/ProductListing";
+import "./page-products.css";
 
 const PageProducts = () => {
     return (
         <div>
             <Navbar />
-            <Sidebar />
+            <div className="products-and-sidebar">
+                <div className="sidebar-container">
+                    <Sidebar />
+                </div>
+                <div className="products-container">
+                    <ProductListing />
+                </div>
+            </div>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
+            <h1>Products</h1>
             <h1>Products</h1>
         </div>
     );
